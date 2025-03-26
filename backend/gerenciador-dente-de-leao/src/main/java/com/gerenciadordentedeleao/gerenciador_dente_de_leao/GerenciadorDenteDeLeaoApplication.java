59589b6dab98740f795a88f19e580c490145454c
Eduarda Brunken Barth <eduarda.b2004@aluno.ifsc.edu.br>
2025-03-25 21:15:20 -0300

    criação do projeto backend

---
+++ backend/gerenciador-dente-de-leao/src/main/java/com/gerenciadordentedeleao/gerenciador_dente_de_leao/GerenciadorDenteDeLeaoApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadordentedeleao.gerenciador_dente_de_leao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDenteDeLeaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDenteDeLeaoApplication.class, args);
+	}
+
+}
---
+++ backend/gerenciador-dente-de-leao/src/test/java/com/gerenciadordentedeleao/gerenciador_dente_de_leao/GerenciadorDenteDeLeaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadordentedeleao.gerenciador_dente_de_leao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDenteDeLeaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
